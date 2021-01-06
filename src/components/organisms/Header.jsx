5333b362618e5adfcc9e6e2f5c21e695221723ca
TEGDV <apjair97@gmail.com>
2021-01-06 10:08:19 -0600

    :see_no_evil: Header constrained to dont break styles in screen sizes over than 920px

--- src/components/organisms/Header.jsx
+++ src/components/organisms/Header.jsx
@@ -1,17 +1,28 @@
 import React from "react";
 import Logo from "../atoms/Logo.jsx";
 import "../../assets/styles/organisms/Header.scss";
-
+import { Link, NavLink } from "react-router-dom";
+import InfoText from "../atoms/InfoText.jsx";
 const Header = () => {
   return (
     <header className="header">
-      <div className="header__container">
-        <h4 className="header__container-item">Products</h4>
-        <h4 className="header__container-item">Portfolio</h4>
-        <Logo className="logo__header" />
-        <h4 className="header__container-item">About</h4>
-        <h4 className="header__container-item">Blog</h4>
-      </div>
+      <nav className="header__container">
+        <NavLink to="/Products" activeClassName="selectedLink">
+          <InfoText text="Products" />
+        </NavLink>
+        <NavLink to="/Portfolio" activeClassName="selectedLink">
+          <InfoText text="Portfolio" />
+        </NavLink>
+        <Link to="/">
+          <Logo className="logo__header" />
+        </Link>
+        <NavLink to="/About" activeClassName="selectedLink">
+          <InfoText text="About" />
+        </NavLink>
+        <NavLink to="/Blog" activeClassName="selectedLink">
+          <InfoText text="Blog" />
+        </NavLink>
+      </nav>
     </header>
   );
 };
--- src/components/templates/Index.jsx
+++ src/components/templates/Index.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import { Route, Link, Switch } from "react-router-dom";
 import Header from "../organisms/Header.jsx";
+import "../../assets/styles/templates/Index.scss";
 const Index = () => {
-  return;
-
-  <Header className="logo_header" />;
+  return (
+    <div className="indexcontainer">
+      <Header className="logo_header" />
+      <Switch>
+        <Route exact path="/Portfolio" />
+        <Route exact path="/Products" />
+        <Route exact path="/About" />
+        <Route exact path="/Blog" />
+      </Switch>
+    </div>
+  );
 };
 
 export default Index;
