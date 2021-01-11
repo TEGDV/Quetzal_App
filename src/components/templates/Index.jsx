ee7c1d4074375fd92a4d44817cdbffb730445b41
TEGDV <apjair97@gmail.com>
2021-01-10 18:43:14 -0600

    :rotating_light: Fix linter issues

--- src/components/templates/Index.jsx
+++ src/components/templates/Index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Link, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Header from "../organisms/Header.jsx";
 import "../../assets/styles/templates/Index.scss";
 import About from "./About.jsx";
